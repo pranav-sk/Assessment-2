15d1cfd85a20fd416920e07105a8c91fecb422dc
pranav-sk <pranav.senthilkumar@rbslearn.net>
2025-07-07 02:17:15 +0000

    update

--- character_test.py
+++ character_test.py
@@ -1,4 +1,4 @@
-from character import Enemy
+from character import Character, Enemy
 
 harry = Enemy( "Harry" ,"A smelly Wumpus")
 
@@ -9,3 +9,7 @@ harry.set_conversation("Come closer. I can't see you!")
 harry.talk()
 
 harry.set_weakness("spaghetti")
+
+print("What will you fight with?")
+fight_with = input()
+harry.fight(fight_with)
