c3bad870c85838c9a30109c6ceaa128034c8c47e
pranav-sk <pranav.senthilkumar@rbslearn.net>
2025-07-20 13:48:55 +0000

    ad

--- character.py
+++ character.py
@@ -5,7 +5,7 @@ class Character:
         self.conversation = None
     
     def describe(self):
-        print(self.char_name  + " is here!")
+        print("Officer" +  self.char_name  + "is here")
         print(self.char_description)
     
     def set_conversation(self, conversation):
@@ -15,27 +15,33 @@ class Character:
         if self.conversation:
             print("[" + self.char_name + " says]: " + self.conversation)
         else:
-            print(self.char_name + " doesn't want to talk to you")   
+            print(self.char_name + " does not want to talk to you")
         
     def fight(self, combat_item):
-        print(self.char_name + " doesn't want to fight you")
+        print(self.char_name + " does not want to fight you")
         return True
     
+
 class Enemy(Character):
     def __init__(self, char_name, char_description):
         super().__init__(char_name, char_description)
-
+        self.weakness = None
+    
     def set_weakness(self, weakness):
         self.weakness = weakness
-
+    
     def fight(self, combat_item):
         if combat_item == self.weakness:
-            print("You have left " + self.char_name + " weak and heartbroken, but have successfully captured him with the " + combat_item + "!" )
+            print("You defeated " + self.char_name + " and successfully captured him with " + combat_item + "!")
             return True
         else:
-            print(self.char_name + " has outsmarted you and has knocked you out, you unworthy officer!")
+            print("You have been outsmarted by " + self.char_name + " and have been defeated!")
             return False
-    
+
+
 class Friend(Character):
     def __init__(self, char_name, char_description):
         super().__init__(char_name, char_description)
+        self.feeling = None
+    def hi_five(self):
+        print(self.char_name + " high fives you back!")
